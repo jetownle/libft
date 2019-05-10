30a0fb50318a3dd2ae1f2fca9f94d8f34dfd7bde
Jeremy Townley <jetownle@e1z2r9p19.42.us.org>
2019-05-09 17:26:36 -0700

    kiss

---
+++ ft_atoi.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jetownle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/08 21:08:33 by jetownle          #+#    #+#             */
+/*   Updated: 2019/05/08 21:38:28 by jetownle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+int		ft_atoi(char *str)
+{
+	int i;
+	int s;
+	int res;
+
+	i = 0;
+	s = 1;
+	res = 0;
+	while (str[i] == ' ' || str[i] == '/r' || str[i] == '\n' || str[i] == '/f' || str[i] == '\t' || str[i] == '\v' )
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+		if (str[i++] == '-')
+			s = -1;
+	while (str[i] >= '0' && str[i] <= '9')
+		res = (res * 10) + (str[i++] - '0');
+	return (res * s);
+}
+
+int main()
+{
+	printf("ft_atoi: %d\n", ft_atoi("123451635436"));
+	printf("atoi: %d\n", atoi("123456"));
+	printf("ft_atoi: %d\n", ft_atoi("12Three45678"));
+	printf("atoi: %d\n", atoi("12Three45678"));
+	printf("ft_atoi: %d\n", ft_atoi("Hello World!"));
+	printf("atoi: %d\n", atoi("Hello World!"));
+	printf("ft_atoi: %d\n", ft_atoi("+42 BLAH!"));
+	printf("atoi: %d\n", atoi("+42 BLAH!"));
+	printf("ft_atoi: %d\n", ft_atoi("-42"));
+	printf("atoi: %d\n", atoi("-42"));
+	printf("ft_atoi: %d\n", ft_atoi("     +42"));
+	printf("atoi: %d\n", atoi("     +42"));
+	printf("ft_atoi: %d\n", ft_atoi("\t\n\v\f\r 42"));
+	printf("atoi: %d\n", atoi("\t\n\v\f\r 42"));
+
+    return 0;
+}
---
+++ ft_memmove.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jetownle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/08 16:47:26 by jetownle          #+#    #+#             */
+/*   Updated: 2019/05/08 17:14:21 by jetownle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+void 	*memmove(void *dst, const void *src, size_t len)
+{
+	void 	*tmp;
+
+	tmp = ft_memalloc(n + 1);
+	tmp = ft_memcpy(tmp, src, n);
+	dest = ft_memcpy(dest, tmp, n);
+	free(tmp);
+	return (dest);
+}
+
