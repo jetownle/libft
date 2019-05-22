9aa17db13e1f6ffc51596dc7aa8474d3afe9c28c
Jeremy Townley <jetownle@e1z2r9p19.42.us.org>
2019-05-22 03:29:25 -0700

    feels good man

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,27 +6,28 @@
 /*   By: jetownle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/08 11:44:49 by jetownle          #+#    #+#             */
-/*   Updated: 2019/05/21 01:58:29 by jetownle         ###   ########.fr       */
+/*   Updated: 2019/05/22 02:52:34 by jetownle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** memcmp returns zero if the two strings are identical, otherwise
+** returns the difference between the first two differing bytes,
+** zero length strings are always identical
+*/
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t	i;
 
 	i = 0;
-	while (((unsigned char *)s1)[i] == ((unsigned char *)s2)[i] && (i < n))
+	while (i < n)
 	{
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 		i++;
 	}
-	if (i == n)
-	{
-		return (0);
-	}
-	else
-	{
-		return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
-	}
+	return (0);
 }
