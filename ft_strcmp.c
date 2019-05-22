792a696019cb56ebd68fcc7bba7bfb3d369045c3
Jeremy Townley <jetownle@e1z2r9p19.42.us.org>
2019-05-22 02:13:01 -0700

    woot

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: jetownle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/30 17:44:22 by jetownle          #+#    #+#             */
-/*   Updated: 2019/05/22 02:02:42 by jetownle         ###   ########.fr       */
+/*   Updated: 2019/05/22 02:12:44 by jetownle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@ int	ft_strcmp(char *s1, char *s2)
 	while (*s1 != '\0' && *s2 != '\0')
 	{
 		if (*s1 != *s2)
-			return ((unsigned char)*s1 - (unsigned char)*s2);
+			return ((unsigned char)*s1 - *s2);
 		s1++;
 		s2++;
 	}
 	if (*s1 != '\0')
 		return ((unsigned char)*s1);
 	if (*s2 != '\0')
-		return ((unsigned char)-*s2);
+		return (-*s2);
 	return (0);
 }
