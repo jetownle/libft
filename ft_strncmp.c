84ae90f8c890ce29601edcffa4ef6278ef859b57
Jeremy Townley <jetownle@e1z2r5p37.42.us.org>
2019-05-24 21:20:30 -0700

    fixed an error

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jetownle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/07 12:50:59 by jetownle          #+#    #+#             */
-/*   Updated: 2019/05/21 21:54:23 by jetownle         ###   ########.fr       */
+/*   Updated: 2019/05/24 21:05:38 by jetownle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	if (i == n)
